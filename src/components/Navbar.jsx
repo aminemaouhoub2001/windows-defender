025a3c6d116167836ea6878303f165045417237e
aminemaouhoub2001 <maouhoubamineofficiel@gmail.com>
2025-04-19 15:56:35 +0100

    fix: reset password URL fallback

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,9 @@ export default function Navbar() {
       <div className="flex items-center space-x-2">
         <img src="/windows-defender.svg" alt="logo" className="h-6" />
         <h1 className="text-lg font-bold bg-gradient-to-r from-cyan-400 via-purple-500 to-green-400 bg-clip-text text-transparent">
-         MMI <span className="font-bold">Evas</span>
+         MMI <span className="font-bold">Evade</span>
         </h1>
-      </div>
+      </div
 
       {/* Navigation Links */}
       <div className="flex items-center space-x-6 text-sm">
