5a7250d6a63664fa05d7ab40689088d8ef1821b6
aminemaouhoub2001 <maouhoubamineofficiel@gmail.com>
2025-04-19 01:13:32 +0100

    fix: reset password URL fallback

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -121,11 +121,9 @@ export default function Home() {
     
     {/* Bloc gauche - translation vers la gauche */}
     <div className="transform translate-x-[-20px]">
-    <div className="flex justify-center items-center h-screen bg-black">
-  <h1 className="text-5xl font-bold bg-gradient-to-r from-cyan-400 via-purple-500 to-green-400 bg-clip-text text-transparent">
-    Evasion <span className="font-bold">Techniques</span>
-  </h1>
-</div>
+    <h1 className="text-3xl md:text-5xl font-bold bg-gradient-to-r from-cyan-400 via-purple-500 to-green-400 bg-clip-text text-transparent">
+  Evasion <span className="font-bold">Techniques</span>
+</h1>
 
       <p className="text-sm mb-4 leading-relaxed">
         Educating professionals on <span className="text-cyan-400">malware evasion techniques</span>
